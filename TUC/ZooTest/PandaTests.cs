98b94f3a2991d6598e3a0e2a7a871946efc98600
Daniel Thyselius <Daniel.Thyselius@gmail.com>
2018-11-26 09:13:00 +0100

    more tests

---
+++ TUC/ZooTest/PandaTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Zoo;
+
+namespace ZooTest
+{
+    public class PandaTests
+    {
+        public PandaTests()
+        {
+
+        }
+
+        [Fact]
+        public void CalculateFood_testingValidPanda_ReturnsCorrectValue()
+        {
+            //arrange
+            var testingPanda = new Panda("klas", 1, 22);
+            //act
+            var result = testingPanda.CalculateFood();
+            //assert
+            Assert.Equal(2, result);
+
+        }
+        
+        [Fact]
+        public void CalculateFood_testingInvalidPanda_ReturnsException()
+        {
+            //arrange
+            var testingPanda = new Panda("klas", -1, 22);
+            //act & assert
+            var ex = Assert.Throws<Exception>(() => testingPanda.CalculateFood());
+
+        }
+        [Fact]
+        public void CalculateFood_testingValidPandaWithAgeAboveSix_ReturnsCorrectValue()
+        {
+            //arrange
+            var testingPanda = new Panda("klas", 7, 22);
+            //act
+            var result = testingPanda.CalculateFood();
+            //assert
+            Assert.Equal(12, result);
+
+        }
+        [Fact]
+        public void CalculateFood_testingValidPandaWithZeroWeight_ReturnsException()
+        {
+            //arrange
+            var testingPanda = new Panda("klas", 7, 0);
+            //act 
+            //&
+            //assert
+            var ex = Assert.Throws<Exception>(() => testingPanda.CalculateFood());
+
+        }
+        [Fact]
+        public void CalculateFood_testingValidPandaWithNegativeWeight_ReturnsException()
+        {
+            //arrange
+            var testingPanda = new Panda("klas", 7, -1);
+            //act 
+            //&
+            //assert
+            var ex = Assert.Throws<Exception>(() => testingPanda.CalculateFood());
+
+        }
+    }
+}
--- TUC/ZooTest/WombatTests.cs
+++ TUC/ZooTest/WombatTests.cs
@@ -25,17 +25,7 @@ namespace ZooTest
             Assert.Equal(19.8, result);
 
         }
-        [Fact]
-        public void CalculateFood_testingValidWombat_ReturnsInCorrectValue()
-        {
-            //arrange
-            var testingWombat = new Wombat("klas", 1, 22);
-            //act
-            var result = testingWombat.CalculateFood();
-            //assert
-            Assert.NotEqual(1, result);
-        }
-
+        
         [Fact]
         public void CalculateFood_testingInvalidWombat_ReturnsException()
         {
@@ -56,9 +46,27 @@ namespace ZooTest
             Assert.Equal(8.8, result);
 
         }
+        [Fact]
+        public void CalculateFood_testingValidWombatWithZeroWeight_ReturnsException()
+        {
+            //arrange
+            var testingWombat = new Wombat("klas", 7, 0);
+            //act 
+            //&
+            //assert
+            var ex = Assert.Throws<Exception>(() => testingWombat.CalculateFood());
 
+        }
+        [Fact]
+        public void CalculateFood_testingValidWombatWithNegativeWeight_ReturnsException()
+        {
+            //arrange
+            var testingWombat = new Wombat("klas", 7, -1);
+            //act 
+            //&
+            //assert
+            var ex = Assert.Throws<Exception>(() => testingWombat.CalculateFood());
 
-
-
+        }
     }
 }
